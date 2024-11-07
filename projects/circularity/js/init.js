4570f3204d9208255fb4daab7a5e1e3da96358c2
ELiesc10 <eli.milan@livingstoncollegiate.org>
2024-11-07 15:43:26 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -33,11 +33,11 @@ var init = function (window) {
                               }
 
         // TODO 3 / 7 : Call the drawCircle() function 
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
+       for(var i = 0; i <= 100; i++) {
+        drawCircle(i);
+
+       }
+       
         
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -50,18 +50,13 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-            physikz.updatePosition(circles[0]);
-            physikz.updatePosition(circles[1]);
-            physikz.updatePosition(circles[2]);
-            physikz.updatePosition(circles[3]);
-            physikz.updatePosition(circles[4]);
+            for(var i = 0; i <= 100; i++) {
+                physikz.updatePosition(circles[i]);
+                game.checkCirclePosition(circles[i]); 
+            }
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-            game.checkCirclePosition(circles[0]); 
-            game.checkCirclePosition(circles[1]); 
-            game.checkCirclePosition(circles[2]); 
-            game.checkCirclePosition(circles[3]); 
-            game.checkCirclePosition(circles[4]); 
+            
 
 
             // TODO 9 : Iterate over the array
@@ -80,9 +75,18 @@ var init = function (window) {
             if ( circle.x > canvas.width ) {
                 circle.x = 0;
             }
+            if (circle.y > canvas.heigth){
+                circle.y= 0; 
+            }
+            if ( circle.x < 0){
+                circle.x= canvas.width;
+            }
+            if (circle.y < 0){
+                circle.y= canvas.heigth;
+            }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
+            we eliminate the function call so as not to repeat the same function and only use a loop
 
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
